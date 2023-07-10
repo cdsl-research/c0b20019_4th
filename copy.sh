48fe37a7108645cda4cb41fdbb8054e40e865111
Naoki Ida <68420999+idanaoki@users.noreply.github.com>
2023-07-10 13:03:19 +0900

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,5 +1,5 @@
 sleep 60
 
-rsync -avh /var/www/test ida@192.168.100.193:~/
+rsync -avh /var/www/test ○○@xx.x.xxx.xxxx:~/
 
-ssh -i ~/.ssh/id_ed25519 ida@192.168.100.193 rm test
+ssh -i ~/.ssh/id_ed25519 ○○@xx.x.xxx.xxxx rm test
